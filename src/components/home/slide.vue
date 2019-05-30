6552bb828a3ed85b32e62ca0557841e147efd1b0
GOLF <golf.prptmk@gmail.com>
2019-05-31 03:27:37 +0700

    slide is now back to overlapped card style

--- src/components/home/slide.vue
+++ src/components/home/slide.vue
@@ -1,29 +1,36 @@
 <template>
-      <mdb-carousel :interval="8000" showControls showIndicators>
-    <mdb-carousel-item img src="https://mdbootstrap.com/img/Photos/Slides/img%20(68).jpg" mask="black-light" alt="First slide">
-      <mdb-carousel-caption title="Light mask" text="First text"></mdb-carousel-caption>
+  <mdb-carousel style="background-color:white;" :interval="8000" showControls showIndicators>
+    <mdb-carousel-item img src="/static/img/homeslide/01.jpg" mask="black-slight" alt="First slide">
+      <!-- <mdb-carousel-caption title="Super light mask" text="First text"></mdb-carousel-caption> -->
     </mdb-carousel-item>
-    <mdb-carousel-item img src="https://mdbootstrap.com/img/Photos/Slides/img%20(6).jpg" mask="black-strong" alt="Second slide">
-      <mdb-carousel-caption title="Strong mask" text="Second text"></mdb-carousel-caption>
+    <mdb-carousel-item
+      img
+      src="/static/img/homeslide/02.jpg"
+      mask="black-slight"
+      alt="Second slide"
+    >
+      <!-- <mdb-carousel-caption title="Super light mask" text="Second text"></mdb-carousel-caption> -->
     </mdb-carousel-item>
-    <mdb-carousel-item img src="https://mdbootstrap.com/img/Photos/Slides/img%20(9).jpg" mask="black-slight" alt="Third slide">
-      <mdb-carousel-caption title="Super light mask" text="Third text"></mdb-carousel-caption>
+    <mdb-carousel-item img src="/static/img/homeslide/03.jpg" mask="black-slight" alt="Third slide">
+      <!-- <mdb-carousel-caption title="Super light mask" text="Third text"></mdb-carousel-caption> -->
+    </mdb-carousel-item>
+    <mdb-carousel-item img src="/static/img/homeslide/04.jpg" mask="black-slight" alt="Third slide">
+      <!-- <mdb-carousel-caption title="Super light mask" text="Third text"></mdb-carousel-caption> -->
     </mdb-carousel-item>
   </mdb-carousel>
 </template>
 
 <script>
-import { mdbCarousel, mdbCarouselItem, mdbCarouselCaption } from 'mdbvue';
-  export default {
-    name: 'HomeSlide',
-    components: {
-      mdbCarousel,
-      mdbCarouselItem,
-      mdbCarouselCaption
-    }
+import { mdbCarousel, mdbCarouselItem, mdbCarouselCaption } from "mdbvue";
+export default {
+  name: "HomeSlide",
+  components: {
+    mdbCarousel,
+    mdbCarouselItem,
+    mdbCarouselCaption
   }
+};
 </script>
 
 <style>
-
 </style>
