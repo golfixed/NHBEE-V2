3523e783fa7a0a386acbea015054901012f2539a
GOLF <golf.prptmk@gmail.com>
2019-05-31 03:28:26 +0700

    reconstruc home page layout

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,14 +1,28 @@
 <template>
-  <div style="padding-top:50px;background-color: #1b1b1b;">
-    <!-- HOME TEST
-    <h3>{{ $t("message.test") }}</h3>-->
-    <!-- HOME SLIDE IS HERE -->
-    <div class="container homeslide">
+  <div class="home-display" style="padding-top:80px;background-color: rgb(51, 51, 51);">
+    <div class="header-bg d-flex align-items-center">
+      <div class="container" style="margin-top: -65px;">
+        <div class="d-flex flex-column">
+          <span class="page-title">{{ $t("message.pageheader.home.title") }}</span>
+          <span class="page-subtitle">{{ $t("message.pageheader.home.subtitle") }}</span>
+        </div>
+      </div>
+    </div>
+    <div class="container page-start" style="overflow:hidden;border-radius: 5px 5px 0 0;">
       <homeslide/>
     </div>
-    <div class="container">
+    <div>
       <homeabout/>
     </div>
+    <div class="container" style="padding:0px;">
+      <survey/>
+    </div>
+    <!-- <div style="padding:0px;">
+      <feature/>
+    </div>
+    <div style="padding:0px;">
+      <update/>
+    </div>-->
   </div>
 </template>
 
@@ -16,11 +30,22 @@
 import layout_default from "@/layouts/main.vue";
 import homeslide from "@/components/home/slide.vue";
 import homeabout from "@/components/home/about.vue";
+import survey from "@/components/home/survey.vue";
+import feature from "@/components/home/features.vue";
+import update from "@/components/home/update.vue";
 export default {
   name: "home_page",
   components: {
     homeslide,
-    homeabout
+    homeabout,
+    survey,
+    feature,
+    update
+  },
+  data() {
+    return {
+      golf: {}
+    };
   },
   created() {
     this.$emit(`update:layout`, layout_default);
@@ -29,7 +54,34 @@ export default {
 </script>
 
 <style scoped>
-.homeslide {
-  height: auto;
+.page-paper {
+  -webkit-box-shadow: -1px 1px 20px -1px rgba(0, 0, 0, 0.75);
+  -moz-box-shadow: -1px 1px 20px -1px rgba(0, 0, 0, 0.75);
+  box-shadow: -1px 1px 20px -1px rgba(0, 0, 0, 0.75);
+}
+.page-start {
+  padding: 0px;
+  margin-top: -70px;
+}
+.home-display {
+  display: grid;
+  grid-template-rows: auto auto auto auto auto auto;
+}
+.header-bg {
+  height: 280px;
+  background-image: url("/static/img/home.jpg");
+  background-position: center top;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+.page-title {
+  color: #e3b800;
+  font-size: 2.5rem;
+  font-weight: bold;
+}
+.page-subtitle {
+  color: #5d5d5d;
+  font-size: 1.8rem;
+  font-weight: bold;
 }
 </style>
