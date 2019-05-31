fa8a57cc4016f1a97d1354f46a03552f24a2d91c
GOLF <golf.prptmk@gmail.com>
2019-05-31 18:21:03 +0700

    news and updates is finished

--- src/components/home/update.vue
+++ src/components/home/update.vue
@@ -1,28 +1,28 @@
 <template>
-  <div class="container-fluid bg" style="padding: 10px;  transition: all 0.3s;">
-    <div class="row" style="width: 500px; margin-bottom:20px;">
-      <h3
-        style="color: white;padding-left: 50px;margin-bottom:0px;font-weight:bold;"
-      >New release - most recent</h3>
+  <div class="container bg">
+    <div class="row" style="margin:0;">
+      <span class="section-text">{{ $t("message.section.news")}}</span>
     </div>
-    <div class="bar" style=" text-align: start; overflow:hidden;">
-      <div class="abt-hover">
-        <h5 class="abt-topic">New1</h5>
-        <p
-          class="abt-paragraph"
-        >To become a center of excellence in research.</p>
+    <div class="update-display">
+      <div class="navigate-icon">
+        <i class="fas fa-chevron-left arrow-color"></i>
       </div>
-      <div class="abt-hover">
-        <h5 class="abt-topic">New2</h5>
-        <p
-          class="abt-paragraph"
-        >Deep tech researches of honeybees for multidisciplinary approach.</p>
+      <div class="row ud-bar">
+        <div class="ud-box">
+          <h5 class="ud-topic">{{ $t("message.update.date")}}</h5>
+          <p class="ud-paragraph">"{{ $t("message.update.topic")}}"</p>
+        </div>
+        <div class="ud-box">
+          <h5 class="ud-topic">{{ $t("message.update.date")}}</h5>
+          <p class="ud-paragraph">"{{ $t("message.update.topic")}}"</p>
+        </div>
+        <div class="ud-box">
+          <h5 class="ud-topic">{{ $t("message.update.date")}}</h5>
+          <p class="ud-paragraph">"{{ $t("message.update.topic")}}"</p>
+        </div>
       </div>
-      <div class="abt-hover">
-        <h5 class="abt-topic">New3</h5>
-        <p
-          class="abt-paragraph"
-        >Research on adaptive behavioral ecology and sociobiology in honeybees.</p>
+      <div class="navigate-icon">
+        <i class="fas fa-chevron-right arrow-color"></i>
       </div>
     </div>
   </div>
@@ -30,41 +30,75 @@
 
 <script>
 export default {
-  name: "homeabout"
+  name: "homeupdate"
 };
 </script>
 
 <style scoped>
+.update-display {
+  display: grid;
+  grid-template-columns: 7% 86% 7%;
+}
+.navigate-icon {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.section-text {
+  color: rgb(194, 194, 194);
+  margin-bottom: 0px;
+  font-weight: bold;
+  font-size: 1.8rem;
+  padding: 30px;
+}
+.ud-box {
+  color: #ffffff;
+  padding: 0px 30px;
+  display: block;
+  text-align: left;
+  width: 33.33%;
+}
+.ud-bar {
+  display: flex;
+  overflow-y: scroll;
+}
+.arrow-color {
+  color: #e5b865;
+}
 .bg {
   background-color: #1d1d1d;
+  width: 100%;
+  display: block;
+  padding: 0 0 30px 0;
 }
-.left-bd {
-  border-left: 5px solid green;
+.ud-topic {
+  color: rgb(150, 150, 150);
+  margin-bottom: 20px;
 }
-.abt-topic {
-  color: #e5b865;
+.ud-paragraph {
+  color: rgb(220, 220, 220);
   font-weight: bold;
 }
-.abt-paragraph {
-  color: white;
-}
-.bar {
-  position: relative;
-  display: flex;
-  transition: all 0.3s;
-}
-.abt-hover {
-  background-color: transparent;
-  padding: 50px 40px;
-  width: 33.33%;
-  transition: all 0.3s;
-  height: 200px;
-}
-.abt-hover:hover {
-  background-color: #2e2e2e;
-  transition: all 0.3s;
-  padding: 50px 40px;
-  width: calc(33.33% + 60px);
-  height: 270px;
+
+@media screen and (max-width: 425px) {
+  .section-text {
+    font-size: 1.5rem;
+    padding: 30px 10px;
+    text-align: center;
+  }
+  .ud-box {
+    color: #ffffff;
+    width: 290px;
+    border: 1px solid #e5b865;
+    flex: 0 0 auto;
+    padding: 20px 20px 5px 20px;
+    margin: 0 15px 0 0;
+  }
+  .ud-bar {
+    display: flex;
+    flex-wrap: nowrap;
+    overflow-x: auto;
+    padding: 0 15px 15px 15px;
+  }
 }
 </style>
