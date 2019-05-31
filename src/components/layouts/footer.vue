f6dff338eb8b01adca93f84e20b658b8884ec811
GOLF <golf.prptmk@gmail.com>
2019-05-31 18:20:48 +0700

    footer is now responsive

--- src/components/layouts/footer.vue
+++ src/components/layouts/footer.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="container-fluid bg">
+  <div class="container-fluid bg" style="padding-top: 70px;">
     <div class="container">
       <div class="row" style="padding: 20px 0px 60px 0px;">
-        <div class="col-6" style="padding: 0px;">
+        <div class="col-lg-6 col-sm-6 col-xs-12" style="padding: 0px;">
           <h4 class="topic" style="padding: 10px 0px;">{{ $t("message.connect.connect_text") }}</h4>
           <div class="d-flex flex-row justify-content-start" style="margin-top: 10px;">
             <div class="p-2">
@@ -20,14 +20,24 @@
                 <mdb-icon fab icon="youtube" class="socialicon youtube"/>
               </a>
             </div>
+            <div class="p-2">
+              <a href="https://www.youtube.com/nhbeekmutt" target="_blank">
+                <mdb-icon fab icon="line" class="socialicon line"/>
+              </a>
+            </div>
           </div>
         </div>
-        <div class="col-6" style="padding: 0px;">
+        <div class="col-lg-6 col-sm-6 col-xs-12" style="padding: 0px;">
           <h4 class="topic" style="padding: 10px 0px;">{{ $t("message.connect.email_text") }}</h4>
           <div class="d-flex flex-row" style="margin-top: 10px;">
             <div class="col" style="padding: 0px 0px 0px 0px;">
               <form class="example" action="/action_page.php">
-                <input style="border: 1px solid white;background-color: #212121;padding: 10px;width: 90%;" type="text" :placeholder="emailsubph" name="search">
+                <input
+                  style="border: 1px solid white;background-color: #212121;padding: 10px;width: 90%;"
+                  type="text"
+                  :placeholder="emailsubph"
+                  name="search"
+                >
                 <button
                   type="submit"
                   style="padding: 10px;border: 1px solid white;width: 10%; float: right;"
@@ -41,14 +51,9 @@
       </div>
       <div class="row" style="padding: 20px 0px; color: white; border-top: 1px solid #e5b864;">
         <div
-          class="col-6"
+          class="col-lg-6 col-sm-6 col-xs-12"
           style="padding: 0px; color:#CBCBCB;"
         >Copyright © Native Honeybee Laboratory 2019</div>
-        <div class="col-6 d-flex justify-content-end" style="padding: 0px; color:#CBCBCB;">
-          <a class="bot-link">Privacy Policy</a>
-          <a class="bot-link">Sitemap</a>
-          <a class="bot-link" style="padding-right:0px !important;">Menu</a>
-        </div>
       </div>
     </div>
   </div>
@@ -111,10 +116,27 @@ export default {
   transition-duration: 0.2s;
   transform: scale(1.1);
 }
+.line {
+  color: #707070;
+  transition-duration: 0.2s;
+}
+.line:hover {
+  color: #359921;
+  transition-duration: 0.2s;
+  transform: scale(1.1);
+}
 .bg {
   background-color: #292929;
 }
 .topic {
   color: white;
+}
+@media screen and (max-width: 425px) {
+  .topic {
+    text-align: center;
+  }
+  .p-2 {
+    width: 25%;
+  }
 }
 </style>
